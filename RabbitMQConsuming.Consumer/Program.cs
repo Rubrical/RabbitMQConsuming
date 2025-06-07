cd1918071eb6fd7476eecbff021b752537dd9402
Filipe Salviano <filipesalviano@proton.me>
2025-06-07 14:36:23 -0300

    Adicionar arquivos de projeto.

---
+++ RabbitMQConsuming.Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { UserName = "admin", HostName = "localhost", Password = "admin", };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+WriteLine("Waiting for messages...");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    WriteLine($"Message received: {message}");
+    await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, false);
+};
+
+await channel.BasicConsumeAsync(queue: "message", autoAck: false, consumer: consumer);
+Console.ReadLine();
---
+++ RabbitMQConsuming.Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { UserName= "admin", HostName = "localhost", Password = "admin",};
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+for (int i = 0; i < 10; i++)
+{
+    var messages = $"{DateTime.Now} - {Guid.NewGuid()}";
+    var body = Encoding.UTF8.GetBytes(messages);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "message",
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body);
+
+    Console.WriteLine($"Message sent: {messages}");
+    await Task.Delay(3000);
+}
